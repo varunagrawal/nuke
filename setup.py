b0c636c54e6f895eb2a26fa5f1809d92512a71dd
Varun Agrawal <varagrawal@gmail.com>
2017-03-25 17:54:40 -0400

    updated setup.py

---
+++ setup.py
@@ -0,0 +1,48 @@
+"""
+Command Line tool to destroy and clean create a directory from scratch.
+"""
+from setuptools import find_packages, setup
+
+dependencies = ['clint']
+
+setup(
+    name='nuke',
+    version='1.0.0',
+    url='https://github.com/varunagrawal/nuke',
+    license='MIT',
+    author='Varun Agrawal',
+    author_email='varunagrawal@gatech.edu',
+    description='Command line tool to nuke a directory',
+    long_description=__doc__,
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=dependencies,
+    entry_points={
+        'console_scripts': [
+            'nuke = nuke.nuke:main',
+        ],
+    },
+    classifiers=[
+        # As from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        # 'Development Status :: 1 - Planning',
+        # 'Development Status :: 2 - Pre-Alpha',
+        # 'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
+        # 'Development Status :: 5 - Production/Stable',
+        # 'Development Status :: 6 - Mature',
+        # 'Development Status :: 7 - Inactive',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX',
+        'Operating System :: MacOS',
+        'Operating System :: Unix',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
