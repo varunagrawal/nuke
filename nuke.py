bb577003a9b6ca89ff0777d2be08413e7ab7efab
Varun Agrawal <varagrawal@gmail.com>
2017-03-25 17:40:35 -0400

    added arg parser and clint prompt

---
+++ nuke.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Main command line tool to nuke a directory.
+"""
+import os
+import os.path as osp
+import shutil
+import argparse
+from clint.textui import colored, puts, prompt
+
+
+def nuke(directory):
+    puts("Nuking " + colored.cyan(directory))
+        
+
+def _argparse():
+    parser = argparse.ArgumentParser("nuke")
+    parser.add_argument("directory", nargs='?', default=os.getcwd(), help="Directory to nuke! Default is current working directory")
+    parser.add_argument("-y", help="Confirm nuking", action="store_true")
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == "__main__":
+    args = _argparse()
+    directory = osp.abspath(args.directory)
+    if prompt.yn("Are you sure you want to nuke this directory?"):
+        nuke(directory)
