1bb3d7a6e40995d5d9a11f9326508ef9a73dc9e7
Varun Agrawal <varagrawal@gmail.com>
2017-03-25 23:01:42 -0400

    fixed current dir bug and conform to style guide

--- nuke/nuke.py
+++ nuke/nuke.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-"""
-Main command line tool to nuke a directory.
-"""
+"""Main command line tool to nuke a directory."""
 import os
 import os.path as osp
 import shutil
@@ -12,37 +10,53 @@ from clint.textui import colored, puts, prompt
 
 
 def nuke(directory):
+    """Nuke the directory specified."""
     puts("Nuking " + colored.cyan(directory))
-        
     try:
-        # Go to the parent directory
-        os.chdir(osp.basename(directory))
-        puts(osp.dirname(directory))
-
         # Nuke the directory
-        shutil.rmtree(directory)
-        os.mkdir(directory)
+
+        # If we have to nuke the current directory,
+        # we simply delete all the contents of the directory
+        if directory == os.getcwd():
+            for x in os.listdir(directory):
+                t = osp.join(directory, x)
+                if osp.isdir(t):
+                    shutil.rmtree(t)
+                else:
+                    os.remove(t)
+
+        # Deleting a specified directory is easier,
+        # but we have to create the directory again
+        else:
+            shutil.rmtree(directory)
+            os.mkdir(directory)
+
     except (FileNotFoundError,):
         puts(colored.yellow("Nuke target does not exist..."))
-    except (Exception,):
+    except (Exception,) as ex:
         puts(colored.yellow("Nuking failed..."))
 
 
 def _argparse():
     parser = argparse.ArgumentParser("nuke")
-    parser.add_argument("directory", nargs='?', default=os.getcwd(), help="Directory to nuke! Default is current working directory")
+    parser.add_argument("directory",
+                        nargs='?',
+                        default=os.getcwd(),
+                        help="Directory to nuke! Default is current directory")
     parser.add_argument("-y", help="Confirm nuking", action="store_true")
     args = parser.parse_args()
     return args
 
 
 def main():
+    """The main function where it all starts."""
     args = _argparse()
     directory = osp.abspath(args.directory)
-    if args.y or prompt.yn("Are you sure you want to nuke this directory?"):
+    if args.y or
+    prompt.yn("Are you sure you want to nuke directory" +
+              colored.blue(directory) + "?"):
         nuke(directory)
 
 
 if __name__ == "__main__":
     main()
-        
