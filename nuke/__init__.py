b653c264e5517ebb4d777c0246030c105891211d
Varun Agrawal <varagrawal@gmail.com>
2017-03-31 22:05:15 -0400

    various module properties now added in a single unified place

---
+++ nuke/__init__.py
@@ -0,0 +1,8 @@
+
+__author__ = "Varun Agrawal"
+__copyright__ = "Copyright 2017."
+__license__ = "MIT"
+__version__ = "1.0.3"
+__maintainer__ = "Varun Agrawal"
+__email__ = "varunagrawal@gatech.edu"
+__status__ = "Production"
