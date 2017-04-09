afd089fb9c0198e0c827c03e041a1684e8764f75
Varun Agrawal <varagrawal@gmail.com>
2017-04-09 14:43:32 -0400

    walk down the directory and get every file while reading the nukeignores

--- nuke/nuke.py
+++ nuke/nuke.py
@@ -2,25 +2,63 @@
 # -*- coding: utf-8 -*-
 
 """Main command line tool to nuke a directory."""
+import argparse
+import fnmatch
 import os
 import os.path as osp
 import shutil
-import argparse
 from clint.textui import colored, puts, prompt
+
+
+def parse_ignore_file(file):
+    with open(file) as ignore_file:
+        ignore_list = ignore_file.readlines()
+    return ignore_list
 
 
 def nuke(directory):
     """Nuke the directory specified."""
     puts("Nuking " + colored.cyan(directory))
+
+    element_list = []
+    ignore_patterns = []
+
+    for (dirpath, dirnames, filenames) in os.walk(directory, topdown=False):
+        element_list.extend([osp.join(dirpath, dn) for dn in dirnames])
+        for fn in filenames:
+            print(fn)
+            if fn == ".nukeignore":
+                print("Found a nukeignore at {}".format(osp.join(dirpath, fn)))
+                ignore_patterns.extend(parse_ignore_file(osp.join(dirpath, fn)))
+                continue
+            element_list.append(osp.join(dirpath, fn))
+
+    # if ".nukeignore" in element_list:
+    #     with open(os.path.join(directory, ".nukeignore")) as nuke_ignore:
+    #         ignore_patterns = nuke_ignore.readlines()
+    #         element_list.remove(".nukeignore")
+
     try:
         # Nuke the directory
         # Just iterate over the contents and delete everything accordingly.
-        for x in os.listdir(directory):
+        print(element_list)
+        print(ignore_patterns)
+        # TODO figure out how to match patterns correctly
+        for pattern in ignore_patterns:
+            for f in element_list:
+                print("Filename: {0}\tPattern: {1}".format(f, pattern))
+                if fnmatch.fnmatch(f, pattern):
+                    print("{0} matches {1}".format(f, pattern))
+            element_list = [n for n in element_list if not fnmatch.fnmatch(n, pattern)]
+        print(element_list)
+
+        for x in element_list:
             t = osp.join(directory, x)
-            if osp.isdir(t):
-                shutil.rmtree(t)
-            else:
-                os.remove(t)
+            # if
+            # if osp.isdir(t):
+            #     shutil.rmtree(t)
+            # else:
+            #     os.remove(t)
 
     except (FileNotFoundError,):
         puts(colored.yellow("Nuke target does not exist..."))
