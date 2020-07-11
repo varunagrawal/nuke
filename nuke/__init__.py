2cf6d2f034afbfa4b709b40e841a4ebea43f4201
Varun Agrawal <varagrawal@gmail.com>
2020-07-11 04:27:38 -0400

    version bump

--- nuke/__init__.py
+++ nuke/__init__.py
@@ -3,7 +3,7 @@
 __author__ = "Varun Agrawal"
 __copyright__ = "Copyright 2017."
 __license__ = "MIT"
-__version__ = "2.4.2"
+__version__ = "2.4.3"
 __maintainer__ = "Varun Agrawal"
 __email__ = "varagrawal@gmail.com"
 __status__ = "Production"
